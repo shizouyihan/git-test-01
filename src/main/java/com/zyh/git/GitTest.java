78d73cfb659601411dc2cdc08f2fd5245b02c1a5
zyh <shizouyihan@163.com>
2022-11-01 10:47:00 +0800

    second commit

--- src/main/java/com/zyh/git/GitTest.java
+++ src/main/java/com/zyh/git/GitTest.java
@@ -8,5 +8,6 @@ package com.zyh.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello,zyh");
+        System.out.println("Hello,git");
     }
 }
