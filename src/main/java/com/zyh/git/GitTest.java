fbb5ce28fa952d53429fb0990e6e4971865c9483
邹艺涵 <shizouyihan@163.com>
2023-02-10 10:07:48 +0000

    gitee更新18：07
    Signed-off-by: 邹艺涵 <shizouyihan@163.com>

--- src/main/java/com/zyh/git/GitTest.java
+++ src/main/java/com/zyh/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("我是2023/2/10");
         System.out.println("18:02");
+        System.out.println("gitee-18：07");
     }
 }
